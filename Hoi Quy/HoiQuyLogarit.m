08cd7b3d60e8b1c852e1816e4eeb451f6c295a59
Huynh Bao Nhat <23207089@student.hcmus.edu.vn>
2025-12-18 14:42:27 +0700

    ham logarit chuan

--- Hoi Quy/HoiQuyLogarit.m
+++ Hoi Quy/HoiQuyLogarit.m
@@ -1,37 +1,28 @@
 function model = HoiQuyLogarit(x, y)
-
-    n = length(x);
-
     if any(x <= 0)
         error('Hoi quy logarit yeu cau x > 0');
     end
-
-    X = log(x);
+    
+    n = length(x);
+    X = log(x); % Chuyển x sang logarit
     Y = y;
 
-    sumX = 0; sumY = 0; sumXY = 0; sumX2 = 0;
+    % Sử dụng tính toán vector để chính xác và nhanh hơn
+    sumX  = sum(X);
+    sumY  = sum(Y);
+    sumXY = sum(X .* Y);
+    sumX2 = sum(X.^2);
 
-    for i = 1:n
-        sumX  = sumX  + X(i);
-        sumY  = sumY  + Y(i);
-        sumXY = sumXY + X(i)*Y(i);
-        sumX2 = sumX2 + X(i)*X(i);
-    end
+    % Công thức tính b và a chuẩn
+    b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX^2);
+    a = (sumY - b * sumX) / n;
 
-    Xm = sumX / n;
-    Ym = sumY / n;
-
-    b = (n*sumXY - sumX*sumY) / (n*sumX^2 - sumX^2);
-    a = Ym - b * Xm;
-
-    st = 0; sr = 0;
-
-    for i = 1:n
-        st = st + (Y(i) - Ym)^2;
-        sr = sr + (Y(i) - (a + b*X(i)))^2;
-    end
-
-    r2 = (st - sr) / st;
+    % Tính hệ số tương quan R^2
+    Y_pred = a + b * X;
+    Y_mean = mean(Y);
+    st = sum((Y - Y_mean).^2);
+    sr = sum((Y - Y_pred).^2);
+    r2 = (st - sr) / st
 
     model.a  = a;
     model.b  = b;
